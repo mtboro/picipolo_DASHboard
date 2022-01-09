42a5bc60561d9a39089ebd2134036c6fc7b3ea2c
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-01-09 22:45:10 +0100

    Update upload page

--- picipolo/pages/data_upload.py
+++ picipolo/pages/data_upload.py
@@ -1,54 +1,28 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from pages import utils
+from pathlib import Path
 
 
 def app():
     st.markdown("## Data Upload")
 
-    # Upload the dataset and save as csv
     st.markdown("### Upload a csv file for analysis.")
     st.write("\n")
 
-    # Code to read a single file
     uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx'])
+
     global data
     if uploaded_file is not None:
         try:
-            data = pd.read_csv(uploaded_file)
+            data = pd.read_csv(uploaded_file, delimiter=';')
         except Exception as e:
             print(e)
             data = pd.read_excel(uploaded_file)
 
-    ''' Load the data and save the columns with categories as a dataframe. 
-    This section also allows changes in the numerical and categorical columns. '''
     if st.button("Load Data"):
+        # Display raw data
+        st.dataframe(data.head())
 
-        # Raw data
-        st.dataframe(data)
-        data.to_csv('data/main_data.csv', index=False)
-
-        # Collect the categorical and numerical columns
-
-        numeric_cols = data.select_dtypes(include=np.number).columns.tolist()
-        categorical_cols = list(set(list(data.columns)) - set(numeric_cols))
-
-        # Save the columns as a dataframe or dictionary
-        columns = []
-
-        # Iterate through the numerical and categorical columns and save in columns
-        columns = utils.genMetaData(data)
-
-        # Save the columns as a dataframe with categories
-        # Here column_name is the name of the field and the type is whether it's numerical or categorical
-        columns_df = pd.DataFrame(columns, columns=['column_name', 'type'])
-        columns_df.to_csv('data/metadata/column_type_desc.csv', index=False)
-
-        # Display columns
-        st.markdown("**Column Name**-**Type**")
-        for i in range(columns_df.shape[0]):
-            st.write(f"{i + 1}. **{columns_df.iloc[i]['column_name']}** - {columns_df.iloc[i]['type']}")
-
-        st.markdown("""The above are the automated column types detected by the application in the data. 
-        In case you wish to change the column types, head over to the **Column Change** section. """)
+        file_path = Path(__file__).resolve()
+        data_path = file_path.parents[2].joinpath('data', 'user_data', 'parsed', 'messengerData.csv')
+        data.to_csv(data_path, index=False, sep=';')
