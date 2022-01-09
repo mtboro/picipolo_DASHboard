84a06ec231dc1a62de7857bdef524b8ca37dfefb
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-01-09 17:35:52 +0100

    Update parser

--- data/data_parser/parser.py
+++ data/data_parser/parser.py
@@ -9,20 +9,21 @@ from datetime import datetime
 from tkinter import Tk
 from tkinter import filedialog
 
-class Parser:
-    __slots__ = ['total_messages', 'path', 'messages_data', 'name']
 
-    def __init__(self, path: str):
+class Parser:
+    __slots__ = ['total_messages', 'path', 'messages_data', 'single_person']
+
+    def __init__(self, path: str, single_person: bool = True):
         self.total_messages = 0
         self.path = path
         self.messages_data = []
-        self.name = 'Facebook User'
+        self.single_person = single_person
 
     @classmethod
     def __get_date(cls, timestamp_ms: int) -> str:
-        '''
+        """
         transform date from ms to year-month-day-hour format
-        '''
+        """
         date = datetime.fromtimestamp(timestamp_ms / 1000)
         return date.strftime("%Y.%m.%d %X")
 
@@ -43,14 +44,20 @@ class Parser:
         return json.load(f, object_hook=fix_unicode)
 
     def export_to_csv(self, name: str) -> None:
-        '''
+        """
         Exports message_data to .csv file
         :param name - name of the file output
-        '''
+        """
         with open(self.path + '/' + name + '.csv', 'w', encoding='utf-8') as parsed_data:
             parsed_data.write('name;who;time;type;text;emoji\n')
 
             for mess in self.messages_data:
+                name = mess['name']
+                name = name.replace('"', '""')
+                name = name.replace(';', ' ')
+                name = name.replace('\n', '')
+                name = name.replace('\r', '')
+
                 who = mess['sender_name']
                 when = Parser.__get_date(mess['timestamp_ms'])
                 emojis = Parser.__get_reactions(mess)
@@ -60,14 +67,14 @@ class Parser:
                 content = content.replace('\n', '')
                 content = content.replace('\r', '')
 
-                row = f'{self.name};{who};{when};{type};{content};{emojis}'
+                row = f'{name};{who};{when};{type};{content};{emojis}'
                 parsed_data.write(f'{row}\n')
 
     def __get_messages_data(self) -> None:
-        '''
+        """
         get: message_data, total amount of messages, conversation name
         by reading files in {self.path}
-        '''
+        """
         i = 0
         while True:
             i += 1
@@ -76,22 +83,54 @@ class Parser:
                 break
             with open(file_path) as json_file:
                 tmp = Parser.parse_json(json_file)
-                self.name = tmp['title']
                 self.messages_data.append(tmp['messages'])
 
         self.messages_data = list(chain.from_iterable(self.messages_data))
         self.total_messages = len(self.messages_data)
 
+    def __get_all_messages_data(self) -> None:
+        """
+        get: message_data, total amount of messages, conversation name
+        by reading files in from folders in {self.path}
+        """
+        for relative_dir in os.listdir(self.path):
+
+            if not os.path.isdir(f'{self.path}/{relative_dir}'):
+                continue
+
+            print(f'parsing {relative_dir}')
+
+            i = 0
+            curr_messages = []
+            while True:
+                i += 1
+                file_path = f'{self.path}/{relative_dir}/message_{i}.json'
+                if not os.path.isfile(file_path):
+                    break
+                with open(file_path) as json_file:
+                    tmp = Parser.parse_json(json_file)
+                    for mess in tmp['messages']:
+                        mess['name'] = tmp['title']
+                    curr_messages.append(tmp['messages'])
+
+            self.messages_data.extend(list(chain.from_iterable(curr_messages)))
+
+        self.total_messages = len(self.messages_data)
+
     def parse_data(self) -> None:
-        self.__get_messages_data()
+        if self.single_person:
+            self.__get_messages_data()
+        else:
+            self.__get_all_messages_data()
+
         self.export_to_csv('messengerData')
 
     @classmethod
     def __get_reactions(cls, message: dict) -> str:
-        '''
+        """
         :return: None or string that is combined with emojis left under specific message
         /w user that gave it
-        '''
+        """
         if "reactions" not in message:
             return ""
         reactions_list = list()
@@ -101,9 +140,9 @@ class Parser:
 
     @classmethod
     def __get_type_and_content(cls, message: dict) -> tuple:
-        '''
+        """
         :returns type of the message, information about specific message type
-        '''
+        """
         if "call_duration" in message:
             return "C", f"{message['call_duration']}"
         if "content" in message:
@@ -122,17 +161,19 @@ class Parser:
             return "U", ""
         return "-", ""
 
+
 def main():
     root = Tk()
     root.withdraw()
     path = filedialog.askdirectory()
     print(f'path: {path}')
-    parser = Parser(path)
+    parser = Parser(path, single_person=False)
 
     parser.parse_data()
 
     df = pd.read_csv(path + '/messengerData.csv', sep=';')
     print(df.head())
+
 
 if __name__ == '__main__':
     main()
