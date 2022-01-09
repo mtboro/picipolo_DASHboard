2f1a75eeca0d811094d7035f04ac14e9c8594118
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-01-09 17:53:04 +0100

    Update heatmap

--- plots/heatmap.py
+++ plots/heatmap.py
@@ -3,28 +3,27 @@ import numpy as np
 
 import pandas as pd
 from pandas.api.types import CategoricalDtype
-from pandas.core.frame import DataFrame
 
 import plotly.express as px
 import plotly.graph_objs as go
 
 from pathlib import Path
 
-
-me = 'Mateusz Borowski'
 day_names = ['Monday', 'Tuesday', 'Wednesday',
-            'Thursday', 'Friday', 'Saturday', 'Sunday']
+             'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 
 def load_data() -> pd.DataFrame:
     file_path = Path(__file__).resolve()
-    data_path = file_path.parents[2].joinpath('data', 'user_data', 'parsed', 'messengerData.csv')
+    data_path = file_path.parents[1].joinpath('data', 'user_data', 'parsed', 'messengerData.csv')
     df = pd.read_csv(data_path, delimiter=';')
 
     df['time'] = pd.to_datetime(df['time'])
 
     return df
 
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+
+def preprocess_data(df: pd.DataFrame, me: str) -> pd.DataFrame:
     weekday_type = CategoricalDtype(categories=day_names, ordered=True)
 
     df['time'] = pd.to_datetime(df['time'])
@@ -37,37 +36,40 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def prepare_data(df: pd.DataFrame, start: datetime, end: datetime) -> pd.DataFrame:
-    df = preprocess_data(df)
+def prepare_data(df: pd.DataFrame, start: datetime, end: datetime, me: str) -> pd.DataFrame:
+    df = preprocess_data(df, me)
 
     df = df[(df['time'] >= start) & (df['time'] <= end)]
 
     df = df.groupby(['day_name', 'hour']).size().reset_index().sort_values(['day_name', 'hour'])
-    df.rename(columns={0: 'number_of_messanges'}, inplace=True)
+    df.rename(columns={0: 'number_of_messages'}, inplace=True)
     return df
 
+
 def create_plotly_array(df: pd.DataFrame) -> np.ndarray:
-    number_of_messanges = df['number_of_messanges'].values
-    return np.array_split(number_of_messanges, 7)
+    number_of_messages = df['number_of_messages'].values
+    return np.array_split(number_of_messages, 7)
 
 
-def create_heatmap(df: pd.DataFrame, start: datetime, end: datetime) -> go.Figure:
-    df = prepare_data(df, start, end)
+def create_heatmap(df: pd.DataFrame, start: datetime, end: datetime, me: str) -> go.Figure:
+    df = prepare_data(df, start, end, me)
     arr = create_plotly_array(df)
 
     return px.imshow(arr,
-                    labels=dict(x="Hour",
-                                y="Day of Week",
-                                color="Number of messages sent"),
-                    y=day_names,
-                    x=[str(i) for i in range(24)]
-                    )
+                     labels=dict(x="Hour",
+                                 y="Day of Week",
+                                 color="Number of messages sent"),
+                     y=day_names,
+                     x=[str(i) for i in range(24)]
+                     )
+
 
 if __name__ == '__main__':
-    df = load_data()
+    data = load_data()
 
-    start = df['time'].min()
+    start = data['time'].min()
     end = datetime.now()
 
-    fig = create_heatmap(df, start, end)
+    me = input('Please provide your full name: ')
+    fig = create_heatmap(data, start, end, me=me)
     fig.show()
