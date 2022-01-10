2bf97f6b70160081f4395d4dcb67bec721c046b8
Szymon-Gut <72514490+Szymon-Gut@users.noreply.github.com>
2022-01-10 15:38:45 +0100

    plots moved to plots folder, code rafactoring

--- picipolo/pages/friends.py
+++ picipolo/pages/friends.py
@@ -6,8 +6,8 @@ from plots import heatmap_friends
 
 
 def app():
-    if 'friends.csv' not in os.listdir('data') or 'friend_requests_received.csv' not in os.listdir('data') or\
-            'friend_requests_sent.csv' not in os.listdir('data') or 'friend_requests_rejected.csv' not in os.listdir('data'):
+    if 'friends.csv' not in os.listdir('data/user_data') or 'friend_requests_received.csv' not in os.listdir('data/user_data') or\
+            'friend_requests_sent.csv' not in os.listdir('data/user_data') or 'friend_requests_rejected.csv' not in os.listdir('data/user_data'):
         st.markdown("Please upload data through `Upload Data` page!")
     else:
         config = {'displayModeBar': False}
