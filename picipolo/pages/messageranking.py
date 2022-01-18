1be172f1eab0acc74cc2a4df89fe4af277dba11b
hbujakow <72279632+hbujakow@users.noreply.github.com>
2022-01-18 12:12:13 +0100

    Update messageranking.py

--- picipolo/pages/messageranking.py
+++ picipolo/pages/messageranking.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
 from pages.plots import utils
 
 def app():
     df = utils.load_data()
-    df = df.groupby('name')['text'].count()   
-    st.write(df)
+    df = df.groupby('name')['text'].count()
+    df = pd.DataFrame(df)
+    df = df.sort_values(by='text', ascending=False)
+    df.columns = ['count']
+    st.write(df.head(20))
     
