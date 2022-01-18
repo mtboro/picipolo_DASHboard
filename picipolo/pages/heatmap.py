443f72c38d997cf9014b69ba3dd0906c6fa678fa
Szymon <72514490+Szymon-Gut@users.noreply.github.com>
2022-01-18 16:52:19 +0100

    Update heatmap.py

--- picipolo/pages/heatmap.py
+++ picipolo/pages/heatmap.py
@@ -11,6 +11,8 @@ def app():
     start = data['time'].min()
     end = datetime.now()
     
+    name = st.text_input('Please provide your name', 'John Doe')
+    
     fig = heatmap.create_heatmap(data, start, end, me=name)
 
     st.plotly_chart(fig, use_container_width=True)
