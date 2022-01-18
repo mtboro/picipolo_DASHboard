282540847ea2f420289aa7ea5b12f27d509c9791
hbujakow <72279632+hbujakow@users.noreply.github.com>
2022-01-18 19:13:06 +0100

    decorate wordclouds

--- picipolo/pages/wordcloudpage.py
+++ picipolo/pages/wordcloudpage.py
@@ -16,16 +16,18 @@ def app():
 
     df = utils.load_data()
 
-    st.markdown("### simple Wordcloud")
-    # count = st.slider("Ilość słów: ", min_value=5, max_value=15)
-    count = 5
+    st.markdown("### Simple Wordcloud")
+    st.markdown("Graphical representations of most commonly used words")
+
+    count = 10
     if(st.button('Reload wordcloud')):
         count = random.randint(15, 25)
-
-    wc = generate_wordcloud.generate_wordcloud(df, count)
+    letters = 1
+    letters = st.slider("Ilość liter:", min_value = 1, max_value=10)
+    wc = generate_wordcloud.generate_wordcloud(df, count, letters)
     st.pyplot(plot_cloud(wc))
 
-    st.markdown("### emoji Wordcloud")
-
+    st.markdown("### Emoji Wordcloud")
+    st.markdown("Most used emojis")
     wc_emoji = generate_emoji_wordcloud.generate_emoji_wordcloud(df)
     st.pyplot(plot_cloud(wc_emoji))
