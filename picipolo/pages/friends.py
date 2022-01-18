68ca33ff2ee62cac69b77c4b6de9d8ea2cb0a298
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-01-18 17:16:56 +0100

    Handle missing data

--- picipolo/pages/friends.py
+++ picipolo/pages/friends.py
@@ -5,10 +5,11 @@ from pages.plots.utils import load_data_bar_chart
 
 
 def app():
-    df_friends_received, df_friends_waiting, df_friends_rejected, df_friends_sent = load_data_bar_chart()
-    if df_friends_received is None or df_friends_waiting is None or df_friends_rejected is None or df_friends_sent:
+    data_bar_chart = load_data_bar_chart()
+    if data_bar_chart is None:
         st.markdown("Please upload data through `Upload Data` page!")
     else:
+        df_friends_received, df_friends_waiting, df_friends_rejected, df_friends_sent = data_bar_chart
         config = {'displayModeBar': False, 'scrollZoom': False}
         st.markdown("## Friends analysis")
         fig1, fig2, fig3, fig4 = bar_chart.create_chart(df_friends_received,
