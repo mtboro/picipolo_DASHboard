b388328aa485a059e3734e93579eec600b07763b
Mateusz Borowski <mateusz.borowski2.stud@pw.edu.pl>
2022-01-18 19:21:49 +0100

    Fix wordcloud

--- picipolo/pages/wordcloudpage.py
+++ picipolo/pages/wordcloudpage.py
@@ -16,18 +16,18 @@ def app():
     if df is None:
         st.markdown("Please upload data through `Upload Data` page!")
     else:
-    st.markdown("### Simple Wordcloud")
-    st.markdown("Graphical representations of most commonly used words")
+        st.markdown("### Simple Wordcloud")
+        st.markdown("Graphical representations of most commonly used words")
 
-    count = 10
-    if(st.button('Reload wordcloud')):
-        count = random.randint(15, 25)
-    letters = 1
-    letters = st.slider("Minimum length of each word:", min_value = 1, max_value=10)
-    wc = generate_wordcloud.generate_wordcloud(df, count, letters)
-    st.pyplot(plot_cloud(wc))
+        count = 10
+        if st.button('Reload Wordcloud'):
+            count = random.randint(15, 25)
 
-    st.markdown("### Emoji Wordcloud")
-    st.markdown("Most used emojis")
-    wc_emoji = generate_emoji_wordcloud.generate_emoji_wordcloud(df)
-    st.pyplot(plot_cloud(wc_emoji))
+        letters = st.slider("Minimum length of each word:", min_value=1, max_value=10, value=3)
+        wc = generate_wordcloud.generate_wordcloud(df, count, letters)
+        st.pyplot(plot_cloud(wc))
+
+        st.markdown("### Emoji Wordcloud")
+        st.markdown("Most used emojis")
+        wc_emoji = generate_emoji_wordcloud.generate_emoji_wordcloud(df)
+        st.pyplot(plot_cloud(wc_emoji))
